fd440af0811767d5c8b6a4247a447a3859aa4329
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-12-11 17:11:00 -0500

    Add some meaningful tests
    This resolves #6.

--- molecule/tests/test_default.py
+++ molecule/tests/test_default.py
@@ -12,11 +12,52 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-# TODO: Add some meaningful tests for this Ansible role.
-#
-# See this issue:
-# https://github.com/cisagov/ansible-role-xfce-cool/issues/6
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize("pkg", ["curl", "python3-lxml"])
+def test_debian_packages(host, pkg):
+    """Test that the appropriate packages were installed on Debian-based OSs other than Debian 9."""
+    if (
+        (
+            host.system_info.distribution == "debian"
+            and host.system_info.codename != "stretch"
+        )
+        or host.system_info.distribution == "kali"
+        or host.system_info.distribution == "ubuntu"
+    ):
+        assert host.package(pkg).is_installed
+
+
+@pytest.mark.parametrize("pkg", ["curl", "python-lxml"])
+def test_debian_9_packages(host, pkg):
+    """Test that the appropriate packages were installed on Debian 9."""
+    if (
+        host.system_info.distribution == "debian"
+        and host.system_info.codename == "stretch"
+    ):
+        assert host.package(pkg).is_installed
+
+
+@pytest.mark.parametrize("pkg", ["python3-lxml"])
+def test_redhat_packages(host, pkg):
+    """Test that the appropriate packages were installed on Fedora."""
+    if host.system_info.distribution == "fedora":
+        assert host.package(pkg).is_installed
+
+
+@pytest.mark.parametrize(
+    "f",
+    [
+        "/etc/xdg/autostart/cool-data-shortcut.desktop",
+        "/usr/share/backgrounds/cool/cisa_cool_retro_0.png",
+        "/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml",
+    ],
+)
+def test_files_exist(host, f):
+    """Test that the appropriate files were copied over or already existed."""
+    assert host.file(f).exists
+
+
+@pytest.mark.parametrize("f", ["/etc/xdg/qterminal.org/qterminal.ini"])
+def test_files_exist_kali(host, f):
+    """Test that the appropriate files were copied over or already existed on Kali."""
+    if host.system_info.distribution == "kali":
+        assert host.file(f).exists
